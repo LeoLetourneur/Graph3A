eeb205313e1c7a7b6bb65a0c78867a6329b1a047
LoicGerland <gerlandloic@gmail.com>
2015-05-25 20:43:51 +0200

    La classe du Poney !

--- src/Sommet.java
+++ src/Sommet.java
@@ -1,3 +1,5 @@
+package src;
+
 import java.util.ArrayList;
 import java.util.List;
 
@@ -9,8 +11,7 @@ public class Sommet {
 	private boolean marque;
 	private int valeur;
     private List<Sommet> sommetAdjacent;
-    private int degresEntrants;
-    private int degresSortants;
+    private int degres;
     
 
     public Sommet(String p_nom, int p_index) {
@@ -69,23 +70,58 @@ public class Sommet {
 		this.sommetAdjacent = sommetAdjacent;
 	}
 
-	public int getDegresEntrants() {
-		return degresEntrants;
+	public int getDegres() {
+		return degres;
 	}
 
-	public void setDegresEntrants(int degresEntrants) {
-		this.degresEntrants = degresEntrants;
-	}
-
-	public int getDegresSortants() {
-		return degresSortants;
-	}
-
-	public void setDegresSortants(int degresSortants) {
-		this.degresSortants = degresSortants;
+	public void setDegres(int degres) {
+		this.degres = degres;
 	}
 	
-	public int getDegresTotals() {
-		return degresSortants + degresEntrants;
+	public static List<Sommet> parcoursLargeur(List<Sommet> graphe, Sommet depart) {
+		List<Sommet> temp = new ArrayList<Sommet>();
+		List<Sommet> traiter = new ArrayList<Sommet>();
+		
+		for(int i = 0; i < graphe.size(); i++) {
+			graphe.get(i).setMarque(false);
+		}
+		
+		depart.setMarque(true);
+		temp.add(depart);
+		
+		while (temp.size() > 0) {
+			Sommet currentSommet = temp.get(0);
+			for(int i = 0; i < currentSommet.getSommetAdjacent().size(); i++) {
+				if(!currentSommet.getSommetAdjacent().get(i).isMarque()) {
+					currentSommet.getSommetAdjacent().get(i).setMarque(true);
+					temp.add(currentSommet.getSommetAdjacent().get(i));
+				}
+			}
+			traiter.add(currentSommet);
+			temp.remove(currentSommet);
+		}
+		return traiter;
+	}
+
+	public static void main(String[] args) {
+		List<Sommet> graphe = new ArrayList<Sommet>();
+		
+		Sommet s1 = new Sommet("a",1);
+		Sommet s2 = new Sommet("b",2);
+		Sommet s3 = new Sommet("c",3);
+		Sommet s4 = new Sommet("d",4);
+		
+		s1.ajouterVoisin(s2);
+		s1.ajouterVoisin(s3);
+		s2.ajouterVoisin(s1);
+		s3.ajouterVoisin(s1);
+		s3.ajouterVoisin(s4);
+		s4.ajouterVoisin(s3);
+		
+		List<Sommet> parcours;
+		
+		parcours = Sommet.parcoursLargeur(graphe, s2);
+		
+		System.out.println(parcours.toString());
 	}
 }
