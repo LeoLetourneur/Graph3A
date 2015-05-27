b6f3ef5d6b10fe78825845da53740868ca62e297
Léo Letourneur <letourneur.leo@gmail.com>
2015-05-27 09:29:02 +0200

    Mouahahahahahahahahahahaah

--- src/Sommet.java
+++ src/Sommet.java
@@ -1,4 +1,3 @@
-
 import java.io.BufferedReader;
 import java.io.FileInputStream;
 import java.io.InputStream;
@@ -6,21 +5,20 @@ import java.io.InputStreamReader;
 import java.util.ArrayList;
 import java.util.List;
 
-
 public class Sommet {
 
     private String nom;
-	private int index;
 	private boolean marque;
+	private boolean couleur;
 	private int valeur;
     private List<Sommet> sommetAdjacent;
     private int degres;
     
 
-    public Sommet(String p_nom, int p_index) {
+    public Sommet(String p_nom) {
         setNom(p_nom);
-        setIndex(p_index);
         setMarque(false);
+        setCouleur(false);
         setValeur(0);
         setSommetAdjacent(new ArrayList<Sommet>());
     }
@@ -39,14 +37,6 @@ public class Sommet {
 
 	public void setNom(String nom) {
 		this.nom = nom;
-	}
-	
-	public int getIndex() {
-		return index;
-	}
-
-	public void setIndex(int index) {
-		this.index = index;
 	}
 	
 	public boolean isMarque() {
@@ -71,6 +61,7 @@ public class Sommet {
 
 	public void setSommetAdjacent(List<Sommet> sommetAdjacent) {
 		this.sommetAdjacent = sommetAdjacent;
+		this.degres = sommetAdjacent.size();
 	}
 
 	public int getDegres() {
@@ -81,12 +72,87 @@ public class Sommet {
 		this.degres = degres;
 	}
 	
-	public static List<Sommet> parcoursLargeur(List<Sommet> graphe, Sommet depart) {
+	public boolean isCouleur() {
+		return couleur;
+	}
+
+	public void setCouleur(boolean couleur) {
+		this.couleur = couleur;
+	}
+	
+	
+	
+	
+	
+	//TODO
+		public static void main(String[] args) {
+			
+			Graph graph = new Graph();
+			graph.lectureFichier("motsdelongueur6.txt");
+			System.out.println("Le nombre de sommet est de "+graph.getSommets().size());
+			System.out.println("Le nombre d'arc est de "+graph.getArcs().size());
+		}
+}
+
+
+
+
+
+//TODO
+class Arc {
+	
+	private Sommet sommet1;
+	private Sommet sommet2;
+	
+	public Arc(Sommet p_sommet1, Sommet p_sommet2) {
+		setSommet1(p_sommet1);
+		setSommet2(p_sommet2);
+	}
+	
+	public Sommet getSommet1() {
+		return sommet1;
+	}
+
+	public void setSommet1(Sommet sommet1) {
+		this.sommet1 = sommet1;
+	}
+
+	public Sommet getSommet2() {
+		return sommet2;
+	}
+
+	public void setSommet2(Sommet sommet2) {
+		this.sommet2 = sommet2;
+	}
+}
+
+
+
+
+
+
+//TODO
+class Graph {
+
+	private String nom;
+	private List<Sommet> sommets;
+	private List<Arc> arcs;
+	
+	public Graph(ArrayList<Sommet> p_sommets, ArrayList<Arc> p_arcs) {
+		setSommets(p_sommets);
+		setArcs(p_arcs);
+	}
+
+	public Graph() {
+		this(new ArrayList<Sommet>(), new ArrayList<Arc>());
+	}
+	
+	public List<Sommet> parcoursLargeur(Sommet depart) {
 		List<Sommet> temp = new ArrayList<Sommet>();
 		List<Sommet> traiter = new ArrayList<Sommet>();
 		
-		for(int i = 0; i < graphe.size(); i++) {
-			graphe.get(i).setMarque(false);
+		for(int i = 0; i < this.getSommets().size(); i++) {
+			this.getSommets().get(i).setMarque(false);
 		}
 		
 		depart.setMarque(true);
@@ -106,9 +172,10 @@ public class Sommet {
 		return traiter;
 	}
 	
-	public static List<Sommet> lectureFichier(String p_path) {
+	public void lectureFichier(String p_path) {
 		
-		ArrayList<Sommet> liste = new ArrayList<Sommet>();
+		this.setSommets(new ArrayList<Sommet>());
+		this.setArcs(new ArrayList<Arc>());
 		
 		try{
 			InputStream ips = new FileInputStream(p_path); 
@@ -116,46 +183,73 @@ public class Sommet {
 			BufferedReader br = new BufferedReader(ipsr);
 			String ligne;
 			String nom;
-			String index;
+			int cpt;
 			
 			while ((ligne=br.readLine())!=null){
-				nom = ligne.split(":")[0];
-				index = ligne.split(":")[1];
-				liste.add(new Sommet(nom, Integer.parseInt(index)));
+				nom = ligne;
+				Sommet sommetCreer = new Sommet(nom);
+				
+				for(Sommet sommetCompare : this.getSommets()) {
+						cpt = 0;
+						for(int i=0;i<6;i++) {
+							if(sommetCreer.getNom().charAt(i) != sommetCompare.getNom().charAt(i)) {
+								cpt++;
+								if(cpt>1)
+									break;
+							}
+						}
+						if(cpt == 1) {
+							sommetCreer.getSommetAdjacent().add(sommetCompare);
+							sommetCompare.getSommetAdjacent().add(sommetCreer);
+							this.getArcs().add(new Arc(sommetCreer, sommetCompare));
+						}
+				}
+				this.getSommets().add(sommetCreer);
 			}
 			br.close(); 
 		}		
 		catch (Exception e){
 			System.out.println(e.toString());
 		}
-		
-		return liste;
+	}
+	
+	public void ajoutSommet(Sommet p_sommet) {
+		getSommets().add(p_sommet);
+	}
+	
+	public void suppressionSommet(Sommet p_sommet) {
+		getSommets().remove(p_sommet);
+	}
+	
+	public int nbSommet() {
+		return getSommets().size();
 	}
 
-	public static void main(String[] args) {
-		List<Sommet> graphe = new ArrayList<Sommet>();
-		
-		Sommet s1 = new Sommet("a",1);
-		Sommet s2 = new Sommet("b",2);
-		Sommet s3 = new Sommet("c",3);
-		Sommet s4 = new Sommet("d",4);
-		
-		s1.ajouterVoisin(s2);
-		s1.ajouterVoisin(s3);
-		s2.ajouterVoisin(s1);
-		s3.ajouterVoisin(s1);
-		s3.ajouterVoisin(s4);
-		s4.ajouterVoisin(s3);
-		
-		List<Sommet> parcours;
-		
-		parcours = Sommet.parcoursLargeur(graphe, s2);
-		
-		System.out.println(parcours.toString());
-		
-		
-		List<Sommet> graph2 = new ArrayList<Sommet>();
-		graph2 = lectureFichier("src/text.txt");
-		System.out.println(graph2.toString());
+	public String toString() {
+        return "Graph "+nom;
+    }
+	
+	public String getNom() {
+		return nom;
+	}
+
+	public void setNom(String nom) {
+		this.nom = nom;
+	}
+
+	public List<Sommet> getSommets() {
+		return sommets;
+	}
+
+	public void setSommets(List<Sommet> sommets) {
+		this.sommets = sommets;
+	}
+
+	public List<Arc> getArcs() {
+		return arcs;
+	}
+
+	public void setArcs(List<Arc> arcs) {
+		this.arcs = arcs;
 	}
 }
